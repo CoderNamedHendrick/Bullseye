8b45f8cf739bb03012a430045e5c4bc3620376c1
hendrick <sebastinesoacatp@gmail.com>
2022-05-21 09:43:10 +0100

    Initial Commit

---
+++ Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Sebastine Odeh on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
